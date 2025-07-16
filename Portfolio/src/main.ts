4c962436f61c60caaa8af7d340a93b64752fc719
Rares Igescu <raresigescu@gmail.com>
2025-07-16 20:06:13 +0300

    Add dependencies for BootStrap Javascript and navbar

--- Portfolio/src/main.ts
+++ Portfolio/src/main.ts
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
