3bc450f915b35f094335ecc41d30364bda183f2e
Rares Igescu <raresigescu@gmail.com>
2025-07-29 20:33:12 +0300

    Add title and angular material buttons

--- Portfolio/src/app/title-hero/title-hero.component.ts
+++ Portfolio/src/app/title-hero/title-hero.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button'
+
 
 @Component({
   selector: 'app-title-hero',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './title-hero.component.html',
   styleUrl: './title-hero.component.scss'
 })
