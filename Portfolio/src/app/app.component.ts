acdf6d3a71a25d2022e0f30b7c635ef9160f31ff
Rares Igescu <raresigescu@gmail.com>
2025-07-30 20:40:35 +0300

    Add personal image and make responsive changes

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TitleHeroComponent } from "./title-hero/title-hero.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [TitleHeroComponent],
+  imports: [TitleHeroComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
