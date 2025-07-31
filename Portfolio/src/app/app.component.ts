9fbc3913999d4a7e5a92f07685125ba04b6e437a
Rares Igescu <raresigescu@gmail.com>
2025-07-31 23:52:20 +0300

    Add social buttons and sample design for second hero title

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { TitleHeroComponent } from "./title-hero/title-hero.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { SecondHeroComponent } from "./second-hero/second-hero.component";
 
 @Component({
   selector: 'app-root',
-  imports: [TitleHeroComponent, HeaderComponent, FooterComponent],
+  imports: [TitleHeroComponent, HeaderComponent, FooterComponent, SecondHeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Portfolio/src/app/title-hero/title-hero.component.ts
+++ Portfolio/src/app/title-hero/title-hero.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button'
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-title-hero',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './title-hero.component.html',
   styleUrl: './title-hero.component.scss'
 })
