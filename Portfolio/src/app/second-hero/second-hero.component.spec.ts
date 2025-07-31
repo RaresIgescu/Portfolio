81b600e7b170b46bf5402bba1daed5cfdb34abb2
Rares Igescu <raresigescu@gmail.com>
2025-07-31 23:51:20 +0300

    Add second hero card

---
+++ Portfolio/src/app/second-hero/second-hero.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SecondHeroComponent } from './second-hero.component';
+
+describe('SecondHeroComponent', () => {
+  let component: SecondHeroComponent;
+  let fixture: ComponentFixture<SecondHeroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SecondHeroComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SecondHeroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Portfolio/src/app/second-hero/second-hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-hero',
+  imports: [],
+  templateUrl: './second-hero.component.html',
+  styleUrl: './second-hero.component.scss'
+})
+export class SecondHeroComponent {
+
+}
