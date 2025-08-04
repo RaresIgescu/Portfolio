e8499b9b2bba9f05302a09aae627bd964d4fa98e
Rares Igescu <raresigescu@gmail.com>
2025-08-04 20:39:36 +0300

    Add color gradient to background and redesign button

--- Portfolio/src/app/second-hero/second-hero.component.ts
+++ Portfolio/src/app/second-hero/second-hero.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-second-hero',
-  imports: [],
+  imports: [MatButtonModule, MatIcon],
   templateUrl: './second-hero.component.html',
   styleUrl: './second-hero.component.scss'
 })
