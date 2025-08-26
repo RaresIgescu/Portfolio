42dc36aed3683de0667634d18cc9648df317a7d4
Rares Igescu <raresigescu@gmail.com>
2025-08-26 17:26:50 +0300

    Changes for overall website functionality

--- Portfolio/src/app/projects/projects.component.ts
+++ Portfolio/src/app/projects/projects.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../core/header/header.component";
+import { FooterComponent } from '../core/footer/footer.component';
+import { TitleProjectsComponent } from "./title-projects/title-projects.component";
+import { ProjectsCardComponent } from "./projects-card/projects-card.component";
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, TitleProjectsComponent, ProjectsCardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
