fdee57c5b0dc8209ba525f3e40a6280da69157bb
Rares Igescu <raresigescu@gmail.com>
2025-11-09 16:41:46 +0200

    Add functionalities for first button in the hero section

--- Portfolio/src/app/home/title-hero/title-hero.component.ts
+++ Portfolio/src/app/home/title-hero/title-hero.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-title-hero',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './title-hero.component.html',
   styleUrl: './title-hero.component.scss'
 })
+
+
 export class TitleHeroComponent {
-  
+  scrollToContact() {
+    const contactSection = document.getElementById('contact');
+    if(contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth'});
+    }
+  }
 }
