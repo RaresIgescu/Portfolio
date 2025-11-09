c483fdb12c2c91a4361a9713ccf19661e3689a9c
Rares Igescu <raresigescu@gmail.com>
2025-11-09 17:55:03 +0200

    Add functionality to forms component

--- Portfolio/src/app/home/form/form.component.ts
+++ Portfolio/src/app/home/form/form.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  formData = {
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  };
 
+  sendEmail() {
+    emailjs.send(
+      'service_392g4y8',
+      'template_6cmo6hj',
+      this.formData,
+      'hWouxVhpuZooCffJh'
+    ).then(() => {
+      alert('Email trimis cu succes!');
+    }).catch(() => {
+      alert('Eroare la trimitere!');
+    });
+  }
 }
