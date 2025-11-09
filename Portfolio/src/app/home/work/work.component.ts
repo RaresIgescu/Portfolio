cb523ccb4f6efb4653033273a3131ec0fe5dbace
Rares Igescu <raresigescu@gmail.com>
2025-11-09 21:56:09 +0200

    Add navigation functions for the my work cards

--- Portfolio/src/app/home/work/work.component.ts
+++ Portfolio/src/app/home/work/work.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-work',
@@ -9,5 +10,17 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './work.component.scss'
 })
 export class WorkComponent {
+  constructor(private router: Router) {}
 
+  goToLenovo() {
+    this.router.navigate(['home', 'lenovo']);
+  }
+
+  goToSTS() {
+    this.router.navigate(['home', 'sts']);
+  }
+
+  goToOrange() {
+    this.router.navigate(['home', 'orange']);
+  }
 }
