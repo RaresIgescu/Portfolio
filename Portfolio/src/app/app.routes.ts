8a06196823260ee206cbe28e47841a764ae8e7bd
Rares Igescu <raresigescu@gmail.com>
2025-11-09 21:55:51 +0200

    Add routes for each of the my work cards

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { importProvidersFrom, NgModule } from '@angular/core';
+import { STSComponent } from './home/sts/sts.component';
+import { LenovoComponent } from './home/lenovo/lenovo.component';
+import { OrangeComponent } from './home/orange/orange.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'projects', component: ProjectsComponent},
-    { path: 'home', component: HomeComponent}
+    { path: 'home', component: HomeComponent},
+    { path: 'home/sts', component: STSComponent},
+    { path: 'home/lenovo', component: LenovoComponent},
+    { path: 'home/orange', component: OrangeComponent}
 ];
 
 export const appConfigProviders = [
